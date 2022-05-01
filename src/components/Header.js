6077d111faea253d0235ad128326d28ac5a41871
Mykyta <mykyta.y@hotmail.com>
2022-05-01 14:07:04 -0300

    working on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Typed from 'react-typed';
 
 const Header = () => {
   return (
     <div className='header-wrapper'>
         <div className='main-info'>
             <h1>Web Development and Website Promotion</h1>
+            <Typed
+                className='typed-text'
+                strings={['Front End Development','Web Develpment', "Web Design"]}
+                typeSpeed={80}
+                backSpeed={40}
+                loop
+            />
+            <a href='#'className='btn-main-offer'>Contact Me</a>
         </div>
     </div>
   )
