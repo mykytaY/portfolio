c69d579df1ebac5b4cc01526d03941d258801367
Mykyta <mykyta.y@hotmail.com>
2022-05-01 23:03:50 -0300

    playing with particles

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,15 @@ const Header = () => {
   return (
     <div className='header-wrapper'>
         <div className='main-info'>
-            <h1>Web Development and Website Promotion</h1>
+            <h1>Hello, I am a</h1>
             <Typed
                 className='typed-text'
-                strings={['Front End Development','Web Develpment', "Web Design"]}
+                strings={['Front End Developer','Web Develper', "Web Designer"]}
                 typeSpeed={80}
                 backSpeed={40}
                 loop
             />
-            <a href='#'className='btn-main-offer'>Contact Me</a>
+            <a href='#'className='btn-main-contact'>Contact Me</a>
         </div>
     </div>
   )
