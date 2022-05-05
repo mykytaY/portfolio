36ae7a1bc98da25bff2febe119da625f1d33aa5c
Mykyta <mykyta.y@hotmail.com>
2022-05-05 15:49:00 -0300

    working on text for about me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -14,9 +14,12 @@ const AboutMe = () => {
           <div className='col-lg-6 col-xm-12'>
               <h1 className='about-heading'>About Me</h1>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in luctus augue. In ullamcorper mi odio, vel euismod enim consectetur vel. Donec vel urna pharetra, ultricies justo vitae, porttitor sem. Etiam quam nunc, posuere sit amet mi id, ultrices tempus purus. In nec mauris et ante imperdiet blandit ac et lacus. In dictum ante eu turpis varius pharetra. Duis rutrum non nisl in pulvinar. Suspendisse auctor turpis eu sem condimentum volutpat. Etiam faucibus elit vel purus posuere, ut dapibus est malesuada. Integer mattis velit laoreet eros dignissim, vitae viverra erat venenatis. Nulla interdum vestibulum libero, ac facilisis diam hendrerit a. Duis nec faucibus orci. Phasellus vel eros non leo tristique varius id at lectus. Vivamus id sagittis orci, sed condimentum dolor. Nullam eget metus in quam congue molestie.
-
-                Phasellus elit odio, vehicula in interdum vitae, laoreet id felis. In ut faucibus elit. Curabitur dapibus condimentum lacus, eget pretium erat fringilla eu. In a dolor nisl. Fusce sodales faucibus elementum. Suspendisse eu lobortis urna. Integer commodo, nisl id tristique maximus, nulla lorem eleifend magna, sit amet mattis quam nunc eu lorem. In posuere porttitor enim, sed tincidunt libero sodales eu. Aenean mattis semper nisi, vel consequat dui suscipit a. Ut congue mattis nulla nec ornare. Quisque molestie lectus ipsum, et dapibus nibh dictum eget. Ut luctus hendrerit dui a ullamcorper. Praesent vel cursus dui.
+                Hello, My name is Mykyta and I love coding and design.
+                I have background in Business and Marketing but I always had love for Tech and Design.
+                Here I am, doing what excites me!
+                I am a Junior Full Stack Developer with interest in Front End Development and Design. I love to see what I make!
+                I am a fast-learner, ambitios, goal-oriented person with an eye for style and attention to detail.
+                I like to communicate and am a good team player but I can also be very independant employee.
               </p>
           </div>
       </div>
